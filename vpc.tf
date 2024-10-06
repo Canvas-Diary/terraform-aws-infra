8d478654b9cf9d020ef277de26533f5acd52f231
kch1015 <songwoo1015@gmail.com>
2024-10-07 04:01:09 +0900

    docs: public subnet 삭제

--- vpc.tf
+++ vpc.tf
@@ -18,18 +18,6 @@ resource "aws_subnet" "public_subnet_1" {
   }
 }
 
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "ap-northeast-2c"
-
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public_subnet_2"
-  }
-}
-
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
