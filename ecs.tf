909038d69d14ab573bd8c33d6cf0514dc363c33e
kch1015 <songwoo1015@gmail.com>
2024-10-07 19:33:17 +0900

    docs: ecr 라이프사이클 정책 설정

--- ecs.tf
+++ ecs.tf
@@ -2,3 +2,21 @@ resource "aws_ecr_repository" "ecr_repository" {
   name                 = "canvas-diary/backend"
   image_tag_mutability = "MUTABLE"
 }
+
+resource "aws_ecr_lifecycle_policy" "example" {
+  repository = aws_ecr_repository.ecr_repository.name
+  policy = data.aws_ecr_lifecycle_policy_document.ecr_lifecycle_policy.json
+}
+
+data "aws_ecr_lifecycle_policy_document" "ecr_lifecycle_policy" {
+  rule {
+    priority    = 1
+    description = "최근 2개 이미지만 저장"
+
+    selection {
+      tag_status      = "any"
+      count_type      = "imageCountMoreThan"
+      count_number    = 2
+    }
+  }
+}
