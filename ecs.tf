41ae660634e032ef79ed96df2eea213da47b6e7e
kch1015 <songwoo1015@gmail.com>
2024-10-07 19:35:51 +0900

    docs: 이름 변경

--- ecs.tf
+++ ecs.tf
@@ -1,14 +1,14 @@
-resource "aws_ecr_repository" "ecr_repository" {
+resource "aws_ecr_repository" "ecr_repo" {
   name                 = "canvas-diary/backend"
   image_tag_mutability = "MUTABLE"
 }
 
-resource "aws_ecr_lifecycle_policy" "example" {
-  repository = aws_ecr_repository.ecr_repository.name
-  policy = data.aws_ecr_lifecycle_policy_document.ecr_lifecycle_policy.json
+resource "aws_ecr_lifecycle_policy" "ecr_lp" {
+  repository = aws_ecr_repository.ecr_repo.name
+  policy = data.aws_ecr_lifecycle_policy_document.ecr_lp_doc.json
 }
 
-data "aws_ecr_lifecycle_policy_document" "ecr_lifecycle_policy" {
+data "aws_ecr_lifecycle_policy_document" "ecr_lp_doc" {
   rule {
     priority    = 1
     description = "최근 2개 이미지만 저장"
