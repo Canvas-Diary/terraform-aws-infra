5c0beb70d95778ab885c7f9f6340454dace98491
kch1015 <songwoo1015@gmail.com>
2024-10-07 21:58:47 +0900

    docs: ecs 태스크 추가

--- ecs.tf
+++ ecs.tf
@@ -24,3 +24,36 @@ data "aws_ecr_lifecycle_policy_document" "leave_recent" {
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = "canvas-diary"
 }
+
+resource "aws_ecs_task_definition" "ecr_deploy_task" {
+  family = "canvas-diary-deploy"
+  requires_compatibilities = ["EC2"]
+  network_mode = "bridge"
+  cpu = 1
+  memory = 500
+  execution_role_arn = "ecsTaskExecutionRole"
+  task_role_arn = "ecsTaskExecutionRole"
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture = "X86_64"
+  }
+
+  container_definitions = jsonencode([
+    {
+      name      = "canvas-diary"
+      image     = "image uri"
+      essential = true
+      cpu       = 1
+      memory    = 500
+      memoryReservation = 500
+      portMappings = [
+        {
+          hostPort      = 80
+          containerPort = 8080
+          protocol = "tcp"
+        }
+      ]
+    }
+  ])
+}
