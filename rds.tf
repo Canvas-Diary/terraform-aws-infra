9db9c3e93d310cba199912956c118eca7480f1cb
kch1015 <songwoo1015@gmail.com>
2024-10-10 10:41:05 +0900

    docs: rds 식별자 설정, 데이터베이스 이름 변경

--- rds.tf
+++ rds.tf
@@ -1,14 +1,15 @@
 resource "aws_db_instance" "rds_instance" {
-  allocated_storage      = 20
-  db_name                = "canvas_diary_mysql"
   engine                 = "mysql"
   engine_version         = "8.0"
-  instance_class         = "db.t4g.micro"
+  identifier             = "rds_mysql"
+  db_name                = "canvas_diary"
   username               = var.db_username
   password               = var.db_password
-  parameter_group_name   = "default.mysql8.0"
+  instance_class         = "db.t4g.micro"
+  allocated_storage      = 20
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.id
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  parameter_group_name   = "default.mysql8.0"
   availability_zone      = "ap-northeast-2a"
   skip_final_snapshot    = true
 }
