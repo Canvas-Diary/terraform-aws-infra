3a58c64dd481aa856a7156daf8910757a4884ee6
kch1015 <songwoo1015@gmail.com>
2024-10-10 16:08:53 +0900

    docs: 항상 버킷 소유자가 객체 소유

--- s3.tf
+++ s3.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "s3_bucket" {
 resource "aws_s3_bucket_ownership_controls" "s3_oc" {
   bucket = aws_s3_bucket.s3_bucket.id
   rule {
-    object_ownership = "BucketOwnerPreferred"
+    object_ownership = "BucketOwnerEnforced"
   }
 }
 
