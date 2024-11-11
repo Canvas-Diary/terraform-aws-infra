6e7ef6c590ef90197713453c35d02b8d441c1f7b
kch1015 <songwoo1015@gmail.com>
2024-11-11 21:50:22 +0900

    docs: S3 CORS 허용

--- s3.tf
+++ s3.tf
@@ -41,3 +41,22 @@ data "aws_iam_policy_document" "public_read_policy_document" {
     ]
   }
 }
+
+resource "aws_s3_bucket_cors_configuration" "example" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = [
+      "http://localhost:5173",
+      "http://www.canvas-diary.kro.kr",
+      "https://www.canvas-diary.kro.kr"
+    ]
+    expose_headers = [
+      "x-amz-server-side-encryption",
+      "x-amz-request-id",
+      "x-amz-id-2"
+    ]
+  }
+}
